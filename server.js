8a8e78957dcfe2aac624c7c3380523ec4a61c0e8
Zachary Clark <zacclark369@gmail.com>
2022-02-09 22:29:12 -0800

    added login and logout functionality

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.superSecret,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
