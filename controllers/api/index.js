da67485807c999c91a90c113c083c135fa711a65
Zachary Clark <zacclark369@gmail.com>
2022-02-09 21:31:17 -0800

    updated to using correct index.js for home-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-const homeRoutes = require('./home-routes.js');
 
-router.use('/', homeRoutes);
+
+
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
